0613ffbdbab2fea6cb4cf592133263b2d8606f7b
Philipp Boersch-Supan <philipp@boersch-supan.de>
2016-10-19 17:43:45 -0400

    added summary method for output

--- R/ucrdtw.R
+++ R/ucrdtw.R
@@ -15,3 +15,18 @@
 ucrdtw <- function(...){
   ucrdtw_ff(...)
 }
+
+
+#' Summarize subsequence search
+#'
+#' Summary method for class ucrdtw
+#'
+#' @param object an object of class ucrdtw
+#' @param ... further arguments passed to or from methods
+#'
+#' @return An unlisted version of the object is returned.
+#' @export
+#'
+summary.ucrdtw <- function(object, ...){
+  unlist(object)
+}
